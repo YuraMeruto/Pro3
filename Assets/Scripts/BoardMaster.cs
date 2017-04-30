16013ea39998eaaa4b37656d0c620c2f37742994
YUUTO\大樹 <yakmsy.memories@ezweb.ne.jp>
2017-04-30 23:04:08 +0900

    ステージを精製まで完了 ポーンはまだ動かせていない

---
+++ Assets/Scripts/BoardMaster.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardMaster : MonoBehaviour
+{
+
+    private GameObject[,] MassObj = new GameObject[10, 10];
+    public int[,] MassNum = new int[10, 10];
+
+    [SerializeField]
+    private int MaxLength;
+    [SerializeField]
+    private int MaxSide;
+
+    private int MaxNumber;
+    //マスのオブジェクト
+    [SerializeField]
+    private GameObject[] ObjMass = new GameObject[2];
+
+    //キャラクターのオブジェクト
+    [SerializeField]
+    private GameObject[] ObjCharcter = new GameObject[2];
+
+    private Vector3 InstancePos = Vector3.zero;
+    // Use this for initialization
+    void Start()
+    {
+        int count = 0;
+        int masscolor = 0;
+        for (int length = 0; length < MaxLength; length++)
+        {
+
+
+            for (int side = 0; side < MaxSide; side++)
+            {
+                InstancePos.x = side;
+                InstancePos.z = length;
+                if (length == 0 && side == 0)
+                    masscolor = 1;
+                GameObject Mass = Instantiate(ObjMass[masscolor], InstancePos, Quaternion.identity) as GameObject;
+                Mass.name = count.ToString();
+                Mass.GetComponent<NumberMass>().SetNumber(count);
+                MassNum[length, side] = count;
+                if (length == 2)
+                {
+                    Vector3 CharacterInstancePos = InstancePos;
+                    CharacterInstancePos.y = 1;
+                    Instantiate(ObjCharcter[0], CharacterInstancePos, Quaternion.identity);
+                }
+                count++;
+                if (masscolor == 0)
+                {
+                    masscolor = 1;
+                }
+                else {
+                    masscolor = 0;
+                }
+
+            }
+            if (masscolor % 2 == 0)
+            {
+                masscolor = 1;
+            }
+            else
+            {
+                masscolor = 0;
+            }
+        }
+        MaxNumber = count;
+    }
+
+    public int GetMaxNumber()
+    {
+        return MaxNumber;
+    }
+
+    
+}
